83ac23d03a73d1bcb80e86b0fa7b6a2628ce5285
01soufiane <130738453+01soufiane@users.noreply.github.com>
2023-06-07 14:03:27 +0100

    Update and rename ChatgptApplication.java to DalleApplication.java

--- demo/src/main/java/com/test/demo/dalle/DalleApplication.java
+++ demo/src/main/java/com/test/demo/dalle/DalleApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ChatgptApplication {
+public class DalleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ChatgptApplication.class, args);
